ab9a39d3fffc8c049f6db8de681cdbd8d54a5131
Oleksandr Bashynskyi <oleksandrbashynskyi@Mac.Dlink>
2026-01-01 18:52:40 +0200

    fix

--- server/api/routers/extension.py
+++ server/api/routers/extension.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form, Request
 from sqlalchemy.orm import Session
 from datetime import datetime
 import uuid
